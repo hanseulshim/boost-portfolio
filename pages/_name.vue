d5152ca20af16f6a90fcdb6fe1d45ff96ce33cb7
Hanseul Shim <hanseulshim@gmail.com>
2020-10-02 15:56:55 -0400

    Add in loader

--- pages/_name.vue
+++ pages/_name.vue
@@ -1,6 +1,10 @@
 <template>
   <div id="container">
-    <div v-if="!iframeLoaded">OSDIFJVZKPMQ</div>
+    <img
+      v-if="!iframeLoaded"
+      id="loader"
+      src="https://s3.amazonaws.com/demo.boostlabs/assets/loader.gif"
+    />
     <iframe
       v-show="iframeLoaded"
       width="100%"
@@ -34,6 +38,10 @@ export default {
 <style lang="scss">
 #container {
   height: 100vh;
+
+  #loader {
+    width: 100%;
+  }
 
   iframe {
     border: none;
