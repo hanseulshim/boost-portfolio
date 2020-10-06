12701cc8eceb5cdefda55c0d2a1612668b2382d0
Hanseul Shim <hanseulshim@gmail.com>
2020-10-05 23:15:43 -0400

    Add auth flow

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,59 +1,46 @@
 <template>
-  <div>
-    <div id="header">
+  <v-app dark>
+    <v-app-bar app :height="115" class="px-5">
       <nuxt-link to="/"><img id="logo" src="~assets/logo.svg" /></nuxt-link>
-      <div v-if="$auth.loggedIn" class="header-row">
-        <nuxt-link v-if="$route.path !== '/'" id="home" to="/">Home</nuxt-link>
-        <button class="link" @click="$auth.logout('google')">sign out</button>
-      </div>
-    </div>
-    <Nuxt />
-  </div>
+      <nuxt-link v-if="$route.path !== '/' && $auth.loggedIn" id="home" to="/"
+        >Home</nuxt-link
+      >
+      <v-spacer />
+      <v-btn
+        v-if="$auth.loggedIn"
+        color="secondary"
+        @click="$auth.logout('google')"
+        >sign out</v-btn
+      >
+    </v-app-bar>
+    <v-main>
+      <v-container fluid>
+        <Nuxt />
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
 <style lang="scss">
 body {
-  margin: 0;
-  background: #b4b4b4;
   font-family: 'Roboto', sans-serif;
-  font-size: 18px;
   font-weight: 400;
 }
-#header {
-  display: flex;
-  align-items: center;
-  background: #161616;
-  padding: 1em 3em;
+
+#logo {
+  width: 200px;
+  cursor: pointer;
+}
+
+.v-application a {
+  text-decoration: none;
   color: #fff;
-  height: 75px;
-
-  .header-row {
-    display: flex;
-    // justify-content: space-between;
-    margin-left: 5em;
-    font-size: 100%;
-    line-height: 4;
-    width: 100%;
-
-    .link {
-      margin-left: auto;
-    }
-  }
-
-  #logo {
-    width: 200px;
-    cursor: pointer;
-  }
-
-  #home {
-    text-decoration: none;
-    color: #fff;
-    &:hover {
-      color: #ff9a0b;
-    }
+  &:hover {
+    color: #ff9a0b;
   }
 }
-#main {
-  padding: 3em;
+
+#home {
+  margin-left: 5em;
 }
 </style>
--- pages/login.vue
+++ pages/login.vue
@@ -1,11 +1,19 @@
 <template>
-  <div>
-    <a class="link" @click="$auth.loginWith('google')">Please log in</a>
-  </div>
+  <v-card class="ma-auto my-12 py-5 px-6" max-width="374" elevation="5">
+    <v-card-title>Boost Labs</v-card-title>
+    <div />
+    <v-card-actions>
+      <v-btn color="primary" block @click="$auth.loginWith('google')"
+        >Login with google</v-btn
+      >
+    </v-card-actions>
+  </v-card>
 </template>
 
 <script>
-export default {}
+export default {
+  middleware: ['loggedIn'],
+}
 </script>
 
 <style></style>
