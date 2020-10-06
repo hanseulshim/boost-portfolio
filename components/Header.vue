d486f66f89affaf3121b9949d16c996d499c4031
Hanseul Shim <hanseulshim@gmail.com>
2020-10-06 16:38:58 -0400

    Add header component

---
+++ components/Header.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-app-bar app :height="115" class="px-5">
+    <nuxt-link to="/"><img id="logo" src="~assets/logo.svg" /></nuxt-link>
+    <nuxt-link v-if="$route.path !== '/' && $auth.loggedIn" id="home" to="/"
+      >Home</nuxt-link
+    >
+    <v-spacer />
+    <v-avatar v-if="$auth.loggedIn" color="red" size="48" class="mr-4">
+      {{ userInitial }}
+    </v-avatar>
+    <v-btn
+      v-if="$auth.loggedIn"
+      color="secondary"
+      @click="$auth.logout('google')"
+      >sign out</v-btn
+    >
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  computed: {
+    userInitial() {
+      return this.$auth.user.given_name[0] + this.$auth.user.family_name[0]
+    },
+  },
+}
+</script>
+
+<style lang="scss">
+#logo {
+  width: 200px;
+  cursor: pointer;
+}
+
+#home {
+  margin-left: 5em;
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,21 +1,6 @@
 <template>
   <v-app dark>
-    <v-app-bar app :height="115" class="px-5">
-      <nuxt-link to="/"><img id="logo" src="~assets/logo.svg" /></nuxt-link>
-      <nuxt-link v-if="$route.path !== '/' && $auth.loggedIn" id="home" to="/"
-        >Home</nuxt-link
-      >
-      <v-spacer />
-      <v-avatar v-if="$auth.loggedIn" color="red" size="48" class="mr-4">
-        {{ userInitial }}
-      </v-avatar>
-      <v-btn
-        v-if="$auth.loggedIn"
-        color="secondary"
-        @click="$auth.logout('google')"
-        >sign out</v-btn
-      >
-    </v-app-bar>
+    <Header />
     <v-main>
       <v-container fluid>
         <Nuxt />
@@ -24,25 +9,10 @@
   </v-app>
 </template>
 
-<script>
-export default {
-  computed: {
-    userInitial() {
-      return this.$auth.user.given_name[0] + this.$auth.user.family_name[0]
-    },
-  },
-}
-</script>
-
 <style lang="scss">
 body {
   font-family: 'Roboto', sans-serif;
   font-weight: 400;
-}
-
-#logo {
-  width: 200px;
-  cursor: pointer;
 }
 
 .v-application a {
@@ -51,9 +21,5 @@ body {
   &:hover {
     color: #ff9a0b;
   }
-}
-
-#home {
-  margin-left: 5em;
 }
 </style>
